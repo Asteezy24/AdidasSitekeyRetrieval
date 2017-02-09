48fb0e1b39d121db8439791cea8ef861c3a88cac
Asteezy24 <alxtv1026@gmail.com>
2017-02-09 14:43:12 -0500

    Initial Commit

---
+++ sitekey.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+import json
+from bs4 import BeautifulSoup
+import requests
+from urllib2 import urlopen
+import urllib2
+
+def getSitekey():
+    url = "http://www.adidas.com/us/men-shoes?sz=120&start=0"
+    shoeLinks = []
+    keyword_for_sitekey = 'data-sitekey'
+    req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+    con = urllib2.urlopen(req)
+    soup = BeautifulSoup(con, "html.parser")
+    data = soup.findAll('div', {'class':'image plp-image-bg'})
+
+    for item in data:
+        link = item.find('a')['href']
+        shoeLinks.append(link)
+
+    for link in shoeLinks:
+        session = requests.Session()
+        session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+            'Accept': 'text/html, application/xhtml+xml, application/xml',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
+            'DNT': '1'
+        })
+        response = session.get(link)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        sitekeyDiv = soup.findAll(attrs={"class": "g-recaptcha"})
+        print "Currently checking for sitekey on " + link
+        if sitekeyDiv:
+            print "\nFound Sitekey on " + link + "\n\n\n"
+            return sitekeyDiv[0]['data-sitekey']
+        else:
+            continue
+    return "Sitekey Not Found"
+
+if __name__ == "__main__":
+    getSitekey()
