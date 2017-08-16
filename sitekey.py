d13592bca8f61be6f112967738f80cd9f003ea99
Asteezy24 <Steeze@macbook-pro.home>
2017-08-16 15:16:01 -0400

    Updated script for python3 capability

--- sitekey.py
+++ sitekey.py
@@ -1,15 +1,20 @@
 from bs4 import BeautifulSoup
-import requests
-from urllib2 import urlopen
-import urllib2
+import requests, time
 
 def getSitekey():
     url = "http://www.adidas.com/us/men-shoes?sz=120&start=0"
+    session = requests.session()
+    session.headers.update({
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Accept': 'text/html, application/xhtml+xml, application/xml',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
+        'DNT': '1'
+    })
     shoeLinks = []
     keyword_for_sitekey = 'data-sitekey'
-    req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
-    con = urllib2.urlopen(req)
-    soup = BeautifulSoup(con, "html.parser")
+    r = session.get(url)
+    soup = BeautifulSoup(r.content, "html.parser")
     data = soup.findAll('div', {'class':'image plp-image-bg'})
 
     for item in data:
@@ -17,26 +22,20 @@ def getSitekey():
         shoeLinks.append(link)
 
     for link in shoeLinks:
-        session = requests.Session()
-        session.headers.update({
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-            'Accept': 'text/html, application/xhtml+xml, application/xml',
-            'Accept-Encoding': 'gzip, deflate, sdch',
-            'Accept-Language': 'en-US,en;q=0.8,da;q=0.6',
-            'DNT': '1'
-        })
         response = session.get(link)
         soup = BeautifulSoup(response.content, 'html.parser')
         sitekeyDiv = soup.findAll(attrs={"class": "g-recaptcha"})
-        print "Currently checking for sitekey on " + link
+        print("Currently checking for sitekey on {}".format(link))
+
         if sitekeyDiv:
-            print "\nFound Sitekey on " + link + "\n\n\n"
-            print "Current Sitekey = " + sitekeyDiv[0]['data-sitekey']
+            print("\nFound Sitekey on {} \n\n\n".format(link))
+            print("Current Sitekey = {}".format(sitekeyDiv[0]['data-sitekey']))
             break
-            
+
         else:
+            time.sleep(2)
             continue
-    print "Sitekey not found."
+    print("Sitekey not found.")
 
 if __name__ == "__main__":
     getSitekey()
